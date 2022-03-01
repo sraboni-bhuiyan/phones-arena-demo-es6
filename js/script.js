f89f147240ceed2e016df7e4e61083e689d577d3
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-03-01 01:09:10 +0100

    changes  added

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ const displaySearchPhone = phones => {
                 <div class="card-body d-flex flex-column justify-content-center">
                     <h5 class="card-title text-center">${phone.brand}</h5>
                     <h6 class="card-title text-center">${phone.phone_name}</h6>
-                    <button class="btn btn-outline-primary" type="button" onclick="loadPhoneDetail(${phone.slug})">See Details</button>
+                    <button class="btn btn-outline-primary" type="button" onclick="loadPhoneDetail('${phone.slug}')">See Details</button>
                 </div>
             </div>
         `;
@@ -32,7 +32,30 @@ const displaySearchPhone = phones => {
 }
 
 const loadPhoneDetail = phoneId => {
-    console.log(phoneId)
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
-    console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPhoneDetail(data.data))
+}
+const displayPhoneDetail = phone => {
+    console.log(phone)
+    const phoneDetail = document.getElementById("phone-detail")
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="${phone.image}" class="img-fluid rounded-start">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h4 class="card-title">${phone.brand}</h4>
+                <h5 class="card-title">${phone.name}</h5>
+                <p class="card-text">Storage: </p>
+                <p class="card-text">Display SIze: </p>
+            </div>
+        </div>
+    </div>
+    `;
+    phoneDetail.appendChild(div)
 }
